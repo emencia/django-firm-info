68b8e7ee0c21fa8803e55c7021bd4570beb09711
Samy Saad <samy@emencia.com>
2024-05-28 10:58:36 +0200

    [DOC] Added singleton manager docstring

--- firm_info/managers.py
+++ firm_info/managers.py
@@ -3,6 +3,17 @@ from django.utils.translation import gettext_lazy as _
 
 
 class SingletonManager(models.Manager):
+    """
+    A manager to ensure that only one instance of the model exists.
+
+    This manager overrides the `create` method to enforce a singleton pattern
+    on the associated model. If an instance of the model already exists,
+    attempting to create another instance will raise a `ValueError`.
+
+    Methods:
+        create(**kwargs): Creates a new instance of the model if none exists.
+            Raises `ValueError` if an instance already exists.
+    """
     def create(self, **kwargs):
         if self.model.objects.exists():
             error_message = _("Model {model_name} has already one instance")
